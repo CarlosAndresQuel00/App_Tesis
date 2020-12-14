44af6e21e141a725d727a7bebe8781740a07fe22
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2020-12-14 01:34:02 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,35 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./users/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./users/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'profile-settings',
+    loadChildren: () => import('./users/profile-settings/profile-settings.module').then( m => m.ProfileSettingsPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./users/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./users/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'notifications',
+    loadChildren: () => import('./users/notifications/notifications.module').then( m => m.NotificationsPageModule)
+  },
+  {
+    path: 'user-profile',
+    loadChildren: () => import('./users/user-profile/user-profile.module').then( m => m.UserProfilePageModule)
   }
+
 ];
 @NgModule({
   imports: [
